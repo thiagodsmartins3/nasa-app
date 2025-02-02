9666d19ab66459d9ec4af68f33043b0265d9e042
thiagodsmartins3 <thiago.ds.martins@tutanota.com>
2025-02-02 00:26:30 -0300

    added search button

--- NASA_App/HomeFeature/HomeView.swift
+++ NASA_App/HomeFeature/HomeView.swift
@@ -12,38 +12,94 @@ struct HomeView: View {
     let store: StoreOf<HomeFeature>
     @Environment(\.modelContext) var modelContext
     @State private var isLiked: Bool = false
+    @State private var showButton: Bool = false
     
     var body: some View {
-        VStack {
-            List(store.state.apodData) {
-                data in
-                Section {
-                    ListRowView(item: data, likedRow: $isLiked)
-                        .id(data.id)
-                        .listRowBackground(
-                            isLiked ?
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(.red, lineWidth: 2) : nil
-                        )
-                }
+        ZStack(alignment: .bottomTrailing) {
+            VStack {
+                List(store.state.apodData) {
+                    data in
+                    Section {
+                        ListRowView(item: data, likedRow: $isLiked)
+                            .id(data.id)
+                            .listRowBackground(
+                                isLiked ?
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.red, lineWidth: 2) : nil
+                            )
+                    }
 
-                
-                if store.state.isLoading {
-                    ProgressView()
-                        .padding()
+                    
+                    if store.state.isLoading {
+                        ProgressView()
+                            .padding()
+                    }
+                    
+                    if data == store.state.apodData.last {
+                        Color.clear
+                            .onAppear {
+                                store.send(.loadMoreData)
+                            }
+                    }
                 }
-                
-                if data == store.state.apodData.last {
-                    Color.clear
-                        .onAppear {
-                            store.send(.loadMoreData)
+                .listRowSpacing(10)
+                .simultaneousGesture(
+                    LongPressGesture(minimumDuration: 0.5).onEnded({ _ in
+                        withAnimation {
+                            showButton.toggle()
                         }
-                }
+                    })
+                )
             }
-            .listRowSpacing(10)
-        }
-        .onAppear() {
-            store.send(.onAppear)
+            .onAppear() {
+                store.send(.onAppear)
+            }
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "magnifyingglass")
+                    .font(.title2.weight(.bold))
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.orange)
+                    .clipShape(Circle())
+                    .shadow(radius: 5, x: 0, y: 3)
+            }
+            .padding()
+            .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            .opacity(showButton ? 1 : 0)
+            
+//            if showButton {
+//                Button {
+//                    
+//                } label: {
+//                    Image(systemName: "magnifyingglass")
+//                        .font(.title2.weight(.bold))
+//                        .foregroundColor(.white)
+//                        .padding()
+//                        .background(.orange)
+//                        .clipShape(Circle())
+//                        .shadow(radius: 5, x: 0, y: 3)
+//                }
+//                .padding()
+//                .transition(.asymmetric(insertion: .scale, removal: .opacity))
+//            } else {
+//                Button {
+//                    
+//                } label: {
+//                    Image(systemName: "magnifyingglass")
+//                        .font(.title2.weight(.bold))
+//                        .foregroundColor(.white)
+//                        .padding()
+//                        .background(.orange)
+//                        .clipShape(Circle())
+//                        .shadow(radius: 5, x: 0, y: 3)
+//                }
+//                .padding()
+//                .transition(.asymmetric(insertion: .scale, removal: .opacity))
+//                .show
+//            }
         }
     }
     
